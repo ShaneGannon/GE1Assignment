62b4a506c86d613eea29b4bd2404243dde5e96df
Shane Gannon <shane.gannon.halo@gmail.com>
2019-12-17 22:24:45 +0000

    have basic tunnel working

---
+++ GE1_Assignemt/Assets/_Scripts/PhylloTunnel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhylloTunnel : MonoBehaviour
+{
+    public Transform _tunnel;
+    public float _tunnelSpeed, _cameraDistance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _tunnel.position = new Vector3(_tunnel.position.x, _tunnel.position.y, _tunnel.position.z + (_tunnelSpeed));
+
+        //This runs on camera so set camera pos to fall behind the tunnel
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, _tunnel.position.z + _cameraDistance);
+    }
+}
