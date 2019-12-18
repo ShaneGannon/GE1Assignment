936cd95f6a258f26efa25bdfdd8ce4e513de389c
Shane Gannon <shane.gannon.halo@gmail.com>
2019-12-18 04:43:48 +0000

    Got the 512 cubes instantiated into a game object correctly

--- GE1_Assignemt/Assets/_Scripts/AudiCubes.cs
+++ GE1_Assignemt/Assets/_Scripts/AudiCubes.cs
@@ -9,12 +9,29 @@ public class AudiCubes : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i < 512; i++)
+        {
+            GameObject _instanceCube = (GameObject)Instantiate(_sampleCubePrefab);
+            //make instance spawn at center of 'spawner'
+            _instanceCube.transform.position = this.transform.position;
+            _instanceCube.transform.parent = this.transform;
+            _instanceCube.name = "ParmetricCube" + i;
+
+            //Want to make a circle 360 degrees / 512 boxes = .703125 degrees per box
+            this.transform.eulerAngles = new Vector3(0, (-0.703125f * i), 0);
+            _instanceCube.transform.position = Vector3.forward * 100;
+
+            //set sample cube objects in list so we can 'talk' to it from the update
+            _sampleCubes[i] = _instanceCube;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < 512; i++)
+        {
+
+        }
     }
 }
